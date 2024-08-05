c73e3773b750b3a074b18722632c3cb9fa510ba8
Kate <166659076+kate00001@users.noreply.github.com>
2024-08-05 15:35:55 +0300

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,5 @@
+how_many_homeworks = 12
+how_many_hours = 1.5
+course_name = 'Python'
+time_for_one_task = how_many_hours / how_many_homeworks
+print('Курс:',course_name + ', всего задач: ' + str(how_many_homeworks)  + ', затрачено часов: ' + str(how_many_hours) + ', среднее время выполнения: ' + str(time_for_one_task))
